a6f9a4af5eebc0c385e1257e8ef58ceb84cec307
zipxing <zipxing@hotmail.com>
2020-08-26 10:01:25 +0800

    add command line argv for asc2editor

--- tool/asc2editor/main.ts
+++ tool/asc2editor/main.ts
@@ -2,7 +2,9 @@ tge.initEnvironment("TERM");
 let am = new AscIIEditor.Model();
 let ar = new AscIIEditor.TermRender();
 let ag = new AscIIEditor.Game(am, ar);
-ag.setAsciiArtFile('test.txt');
+if(process.argv.length==3) {
+    ag.setAsciiArtFile(process.argv[2]);
+}
 ag.initGame();
 ag.regKeyAction({'s':'S'});
 ag.loop();
