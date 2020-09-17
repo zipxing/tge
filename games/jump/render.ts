d2d0e6cb2c973e878509f33566337f5e49cc3465
zipxing <zipxing@hotmail.com>
2020-09-17 17:04:58 +0800

    coding jump

--- games/jump/render.ts
+++ games/jump/render.ts
@@ -2,7 +2,7 @@ namespace Jump {
     export class TermRender extends tge.Render {
         titlebox: any;
         logobox: any;
-        gamebox: any;
+        //gamebox: any;
         carbox: any;
         //msgbox: any;
 
@@ -10,13 +10,13 @@ namespace Jump {
             super();
 
             tge.AscIIManager.loadArtFile("ascii_art/tge.txt", "tgelogo");
-            tge.AscIIManager.loadArtFile("ascii_art/snake.txt", "snake");
+            tge.AscIIManager.loadArtFile("ascii_art/jump.txt", "jump");
 
             let nb = (<tge.TermRun>tge.env);
 
             this.titlebox = nb.blessed.box({
                 width:Model.gamew+2,
-                height:4,
+                height:6,
                 top:0,
                 left:3,
                 tags:true
@@ -26,7 +26,7 @@ namespace Jump {
             this.logobox = nb.blessed.box({
                 width:12,
                 height:4,
-                top:0,
+                top:1,
                 left:Model.gamew+16,
                 tags:true
             });
@@ -37,7 +37,7 @@ namespace Jump {
                 height:Model.carh,
                 top: 20 + 4,
                 left: 3,
-                border:{type:'line', fg:238},
+                border:{type:'line', fg:38},
             });
             nb.tscreen.append(this.carbox);
 
@@ -55,9 +55,9 @@ namespace Jump {
         }
 
         drawTitle() {
-            let s = tge.AscIIManager.getArt("snake").blessed_lines;
+            let s = tge.AscIIManager.getArt("jump").blessed_lines;
             //let st = tge.Timer.getStage("Jump.Timer.Title");
-            this.titlebox.setContent(`${s[0]}\n${s[1]}\n${s[2]}\n${s[3]}`);
+            this.titlebox.setContent(`${s[0]}\n${s[1]}\n${s[2]}\n${s[3]}\n${s[4]}\n${s[5]}`);
         }
 
         drawLogo() {
