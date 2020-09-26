b2e836b1a7fe48c621db42cda827b68185938696
zipxing <zipxing@zipxing-newmac.local>
2020-09-26 18:52:44 +0800

    city model work

---
+++ games/city/main.ts
@@ -0,0 +1,24 @@
+namespace City {
+    export function main(runtype: string) {
+        /*tge.initEnvironment(runtype);
+        tge.bindLogPath('tmp/snake.log');
+        let m = new Snake.Model();
+        let r = new Snake.TermRender();
+        let g = new Snake.Game(m, r);
+        g.initGame();
+        g.regKeyAction({
+            'i':'W', 'k':'S', 'j':'A', 'l':'D', 'r':'R'});
+        g.loop();*/
+        let m = new City.Model();
+        m.searchUnit();
+        console.log(m.unit_map);
+        console.log("--------------");
+        console.log(m.units);
+    }
+    if((typeof window) !== 'undefined') {
+        //browser term...
+        window.onload = () => { main("WEBTERM"); }
+    } else {
+        main("TERM");
+    }
+}
---
+++ games/city/model.ts
@@ -0,0 +1,78 @@
+namespace City {
+    export interface Cell {
+        id: number;
+        color: number;
+        level: number;
+    }
+    export interface Unit {
+        id: number;
+        cells: {[key: number]: any};
+    }
+    export class Model extends tge.Model {
+        static cityw:number = 5;
+        static cityh:number = 5;
+
+        unit_map: { [key: number]: Unit} = {};
+        units: {[key: number]: Unit} = {};
+        grid: Cell[][];
+
+        constructor() {
+            super();
+            this.grid = [];
+            for(let i=0; i<Model.cityh; i++) {
+                this.grid[i]=[];
+                for(let j=0; j<Model.cityw; j++)
+                    this.grid[i][j]={
+                        id: i*Model.cityw+j,
+                        color: (Math.floor((Math.random()*4)+1)),
+                        level: 0
+                    };
+            }
+        }
+
+        checkLianTong(x:number, y:number, color:number, u:Unit) {
+            if(x<0 || y<0 || x>=Model.cityw || y>=Model.cityh)
+                return;
+            let c = this.grid[y][x];
+            if(c.color == color) {
+                this.unit_map[c.id] = u;
+                u.cells[c.id] = c.id;
+            }
+        }
+
+        searchUnit() {
+            for(let i=0; i<Model.cityh; i++) {
+                for(let j=0; j<Model.cityw; j++) {
+                    let c = this.grid[i][j];
+                    let cur_unit = null;
+                    if(c.id in this.unit_map) {
+                        cur_unit = this.unit_map[c.id];
+                    } else {
+                        cur_unit = <Unit> {
+                            id: c.id,
+                            cells:{}
+                        };
+                        cur_unit.cells[c.id] = c.id;
+                        this.unit_map[c.id] = cur_unit;
+                    }
+                    let y = Math.floor(c.id / Model.cityw);
+                    let x = c.id % Model.cityw;
+                    let dd = [
+                        [0, -1], //up
+                        [0, 1],  //down
+                        [-1, 0], //left
+                        [1, 0]   //right
+                    ];
+                    for(let n=0; n<4; n++)
+                        this.checkLianTong(x+dd[n][0], y+dd[n][1], c.color, cur_unit);
+                }
+            }
+            for(let o in this.unit_map) {
+                let uid = this.unit_map[o].id;
+                if(!(uid in this.units)) {
+                    this.units[uid] = this.unit_map[o];
+                }
+            }
+        }
+    }
+}
